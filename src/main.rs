d6c5fa629c3889405eb32493ff74b365409876a1
Mohan Gupta <mohan.kumar.gupta@gmail.com>
2022-08-28 16:04:39 +1000

    working now

--- src/main.rs
+++ src/main.rs
@@ -100,32 +100,6 @@ fn start_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let timeout = std::time::Duration::from_millis(100);
-
-    println!("Check terminal background color");
-    let term = termbg::terminal();
-    let rgb = termbg::rgb(timeout);
-    let theme = termbg::theme(timeout);
-
-    println!("  Term : {:?}", term);
-    match rgb {
-        Ok(rgb) => {
-            println!("  Color: R={:x}, G={:x}, B={:x}", rgb.r, rgb.g, rgb.b);
-        }
-        Err(e) => {
-            println!("  Color: detection failed {:?}", e);
-        }
-    }
-
-    match theme {
-        Ok(theme) => {
-            println!("  Theme: {:?}", theme);
-        }
-        Err(e) => {
-            println!("  Theme: detection failed {:?}", e);
-        }
-    }
-
     // first things first
     prerequisites();
 
